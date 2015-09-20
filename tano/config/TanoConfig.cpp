bb3dfa0e943f53270d8469c2db42d2241b4702bd
Tadej Novak <tadej@tano.si>
2015-09-20 11:30:19 +0200

    Project reorganisation - part 2

--- tano/config/TanoConfig.cpp
+++ tano/config/TanoConfig.cpp
@@ -16,9 +16,9 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *****************************************************************************/
 
-#include "ConfigPlayer.h"
 #include "core/Common.h"
 
+#include "TanoConfigGenerated.h"
 #include "TanoConfig.h"
 
 QString TanoConfig::applicationDataDir() const
